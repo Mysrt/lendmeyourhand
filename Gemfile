b511044e786d151b75b664888c3160036ff9bf97
James Trowbridge <trowbridge.james@gmail.com>
2012-04-03 01:11:34 -0400

    use amazon s3 for file storage

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.1'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'aws-sdk', '~> 1.3.4'
 gem 'pg'
 gem 'paperclip'
 
--- app/models/hand.rb
+++ app/models/hand.rb
@@ -1,7 +1,15 @@
 class Hand < ActiveRecord::Base
   include Paperclip::Glue
 
-  has_attached_file :picture, :styles => {:large => '600x600>', :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :picture, 
+    :styles => {:large => '600x600>', 
+                :medium => "300x300>", 
+                :thumb => "100x100>" },
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :path => ":sytle/:id/:filename.:extension",
+    :bucket => 'lendmeyourhand'  
+
 
   validates_presence_of :name
   validates_attachment_presence :picture
